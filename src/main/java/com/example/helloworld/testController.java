8ce7bce91677ff0a5b6d14a7f5c581c5731f6fd9
yashaswini-k-gowda <yashaswini1ga14ec158@gmail.com>
2019-07-10 20:21:51 +0530

    initial

---
+++ src/main/java/com/example/helloworld/testController.java
@@ -0,0 +1,15 @@
+package com.example.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class testController {
+
+	@RequestMapping("/welcome")
+	public ModelAndView firstPage() {
+		return new ModelAndView("welcome");
+	}
+
+}
